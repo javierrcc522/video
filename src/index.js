1b7d607a854bdaef94d342126813506937ceec96
Javier Jimenez <javierrcc522@gmail.com>
2018-09-16 12:15:57 -0700

    fix env problem with react-native package

--- src/index.js
+++ src/index.js
@@ -4,9 +4,10 @@ import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
 
+import { REACT_APP_API_KEY } from 'react-native-dotenv'
 
 
-const API_KEY = `${process.env.REACT_APP_API_KEY}`;
+const API_KEY = `${REACT_APP_API_KEY}`;
 
 YTSearch({key: API_KEY, term: 'surfboards'}, function(data){
   console.log(data);
