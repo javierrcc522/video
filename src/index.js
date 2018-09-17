7b4caeb7e7d7b606631235d172763f9a02b7b927
Javier Jimenez <javierrcc522@gmail.com>
2018-09-16 19:22:57 -0700

    refactor functional to class and setState of video

--- src/index.js
+++ src/index.js
@@ -1,22 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
-
 import SearchBar from './components/search_bar';
-
 import { REACT_APP_API_KEY } from 'react-native-dotenv'
+const API_KEY = REACT_APP_API_KEY;
 
+class App extends Component {
+  constructor(props){
+    super(props);
 
-const API_KEY = `${REACT_APP_API_KEY}`;
+    this.state = { videos: [] };
 
-YTSearch({key: API_KEY, term: 'surfboards'}, function(data){
-  console.log(data);
-})
+    YTSearch({key: API_KEY, term: 'surfboards'}, (data) => {
+      this.setState({ videos });
+      //this.setState({ videos: videos})
+    });
+  }
 
-const App = () => {
-  return <div>
-  <SearchBar />
-  </div>
-};
+  render() {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(<App />, document.querySelector(".container"));
